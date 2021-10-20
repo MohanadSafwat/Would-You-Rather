80e30e16053278d5b3e9d00aaf7627a8160de3d5
MohanadSafwat <57914175+MohanadSafwat@users.noreply.github.com>
2021-10-20 03:02:55 +0200

    create-react-app

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { handleInitialData } from "../actions/shared";
+import Dashboard from "./Dashboard";
+class App extends Component {
+  componentDidMount() {
+    this.props.dispatch(handleInitialData);
+  }
+  render() {
+    return <div>{this.props.loading ? <h1>hi</h1> : <Dashboard />}</div>;
+  }
+}
+
+function mapStateToProps({ authedUser }) {
+  return {
+    loading: authedUser === null,
+  };
+}
+
+export default connect(mapStateToProps)(App);
